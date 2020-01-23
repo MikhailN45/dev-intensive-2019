9192b841c356fa0fa87e6b44a47bc22ad10e6104
Mike <shelove6391@gmail.com>
2020-01-23 12:43:27 +0500

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,38 +1,74 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.abs
 
-
-const val SECOND = 1000L
-const val MINUTE = 60 * SECOND
-const val HOUR = 60 * MINUTE
-const val DAY = 24 * HOUR
-
-fun Date.format(pattern: String = "HH:mm:ss dd:MM:yy"): String {
-    val dateFormat = java.text.SimpleDateFormat(pattern, Locale("ru"))
-    return dateFormat.format(this)
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
+    val formatter = SimpleDateFormat(pattern, Locale("ru"))
+    return formatter.format(this)
 }
 
-fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
-    var time = this.time
+fun Date.add(value: Int, units: TimeUnits): Date {
+    return Date(this.time + value * units.value)
+}
 
-    time += when (units) {
-        TimeUnits.SECOND -> value * SECOND
-        TimeUnits.MINUTE -> value * MINUTE
-        TimeUnits.HOUR -> value * HOUR
-        TimeUnits.DAY -> value * DAY
+fun Date.humanizeDiff(): String {
+    val diff = (this.time - Date().time)
+    return when (abs(diff)) {
+        in 0..1 * TimeUnits.SECOND.value -> "только что"
+        in 1 * TimeUnits.SECOND.value..45 * TimeUnits.SECOND.value -> if (diff > 0) "через несколько секунд" else "несколько секунд назад"
+        in 45 * TimeUnits.SECOND.value..75 * TimeUnits.SECOND.value -> if (diff > 0) "через минуту" else "минуту назад"
+        in 75 * TimeUnits.SECOND.value..45 * TimeUnits.MINUTE.value -> when (abs(diff / TimeUnits.MINUTE.value)) {
+            1L -> if (diff > 0) "через минуту" else "минуту назад"
+            in 2..4 -> if (diff > 0) "через ${diff / TimeUnits.MINUTE.value} минуты" else "${abs(
+                diff / TimeUnits.MINUTE.value
+            )} минуты назад"
+            else -> if (diff > 0) "через ${diff / TimeUnits.MINUTE.value} минут" else "${abs(diff / TimeUnits.MINUTE.value)} минут назад"
+        }
+        in 45 * TimeUnits.MINUTE.value..75 * TimeUnits.MINUTE.value -> "час назад"
+        in 75 * TimeUnits.MINUTE.value..22 * TimeUnits.HOUR.value -> when (abs(diff / TimeUnits.HOUR.value)) {
+            1L -> if (diff > 0) "через час" else "час назад"
+            in 2..4 -> if (diff > 0) "через ${abs(diff / TimeUnits.HOUR.value)} часа" else "${abs(
+                diff / TimeUnits.HOUR.value
+            )} часа назад"
+            else -> if (diff > 0) "через ${abs(diff / TimeUnits.HOUR.value)} часов" else "${abs(diff / TimeUnits.HOUR.value)} часов назад"
+        }
+        in 22 * TimeUnits.HOUR.value..26 * TimeUnits.HOUR.value -> "день назад"
+        in 26 * TimeUnits.HOUR.value..360 * TimeUnits.DAY.value -> when (abs(diff / TimeUnits.DAY.value)) {
+            1L -> if (diff > 0) "через день" else "день назад"
+            in 2..4 -> if (diff > 0) "через ${abs(diff / TimeUnits.DAY.value)} дня" else "${abs(diff / TimeUnits.DAY.value)} дня назад"
+            else -> if (diff > 0) "через ${abs(diff / TimeUnits.DAY.value)} дней" else "${abs(diff / TimeUnits.DAY.value)} дней назад"
+        }
+        else -> if (diff / TimeUnits.DAY.value > 0) "более чем через год" else "более года назад"
     }
-    this.time = time
-    return this
 }
 
-fun Date.humanizeDiff(date: Date = Date()): String {
-return String()
-}
+enum class TimeUnits(val value: Long) {
+    SECOND(1000L), MINUTE(60 * SECOND.value), HOUR(60 * MINUTE.value), DAY(24 * HOUR.value);
 
-enum class TimeUnits {
-    SECOND,
-    MINUTE,
-    HOUR,
-    DAY
+    fun plural(value: Int): String {
+        return "$value ${when (this) {
+            SECOND -> when (value % 10) {
+                1 -> "секунду"
+                in 2..4 -> "секунды"
+                else -> "секунд"
+            }
+            MINUTE -> when (value % 10) {
+                1 -> "минуту"
+                in 2..4 -> "минуты"
+                else -> "минут"
+            }
+            HOUR -> when (value % 10) {
+                1 -> "час"
+                in 2..4 -> "часа"
+                else -> "часов"
+            }
+            DAY -> when (value % 10) {
+                1 -> "день"
+                in 2..4 -> "дня"
+                else -> "дней"
+            }
+        }}"
+    }
 }
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,27 +1,79 @@
 package ru.skillbranch.devintensive.utils
 
+import java.util.*
+
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
-        val parts: List<String>? = fullName?.split(" ")
+        val name = fullName?.trim()
 
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        if (name == null || name.isEmpty()) {
+            return Pair(null, null)
+        }
 
-        return firstName to lastName
+        val parts = name.split(" ")
+        return Pair(parts.getOrNull(0), parts.getOrNull(1))
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
+        val string = payload.replace(" ", divider)
+        val map = mapOf(
+            "а" to "a",
+            "б" to "b",
+            "в" to "v",
+            "г" to "g",
+            "д" to "d",
+            "е" to "e",
+            "ё" to "e",
+            "ж" to "zh",
+            "з" to "z",
+            "и" to "i",
+            "й" to "i",
+            "к" to "k",
+            "л" to "l",
+            "м" to "m",
+            "н" to "n",
+            "о" to "o",
+            "п" to "p",
+            "р" to "r",
+            "с" to "s",
+            "т" to "t",
+            "у" to "u",
+            "ф" to "f",
+            "х" to "h",
+            "ц" to "c",
+            "ч" to "ch",
+            "ш" to "sh",
+            "щ" to "sh'",
+            "ъ" to "",
+            "ы" to "i",
+            "ь" to "",
+            "э" to "e",
+            "ю" to "yu",
+            "я" to "ya"
+        )
 
+        return buildString {
+            for (char in string) {
+                val res = when {
+                    char.isUpperCase() -> map[char.toLowerCase().toString()]?.toUpperCase(Locale.ENGLISH)
+                    else -> map[char.toString()]
+                }
+                if (res != null)
+                    append(res)
+                else
+                    append(char)
+            }
 
-
-        return String()
+        }
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-//        val nameFirstLetter: String? = firstName?.get(0)?.toUpperCase().toString()
-//        val surnameFirstLetter: String? = lastName?.get(0)?.toUpperCase().toString()
-//        return nameFirstLetter + surnameFirstLetter
-        return String()
+        val nameFirstLetter = firstName?.trim()?.getOrNull(0)?.toUpperCase()
+        val surnameFirstLetter = lastName?.getOrNull(0)?.toUpperCase()
+        return if (nameFirstLetter != null && surnameFirstLetter != null) "$nameFirstLetter$surnameFirstLetter"
+        else if (nameFirstLetter != null) "$nameFirstLetter"
+        else if (surnameFirstLetter != null) "$surnameFirstLetter"
+        else null
     }
 
 }
